99c8f710fab60099e8c0cc943e0e468630046165
Alex Paredes <alexparedesr95@gmail.com>
2022-02-06 21:18:43 -0600

    FEAT: Primer commit.

---
+++ src/views/CounterView.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>
+        Counter View: {{ counterStore.counter }}!!!!
+    </h1>
+
+    <button @click="counterStore.incrementBy(1)">+1</button>
+    <button @click="counterStore.incrementBy(2)">+2</button>
+    <button @click="increaseBy(5)">+5</button>
+</template>
+
+<script lang="ts" setup>
+import { useCounterStore } from '../stores/counter';
+
+const counterStore = useCounterStore();
+
+const increaseBy = ( value: number ) => {
+    counterStore.incrementBy( value );
+};
+
+</script>
